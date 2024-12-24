47e1890cf9398c36ae4b6f3acfcbcfb989f8d32b
mbaek01 <marcusbaek248@gmail.com>
2024-12-25 03:17:54 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="attention_map_diffusers",
-    version="0.1.4",
+    version="0.1.5",
     author="wooyeolbaek",
     author_email="100wooyeol@gmail.com",
     description="attention map for diffusers",
